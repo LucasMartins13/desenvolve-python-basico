7438a89615f99b987af81470078b3c5dc193ec32
LucasMartins13 <166061972+LucasMartins13@users.noreply.github.com>
2024-06-12 17:13:02 -0300

    Add files via upload

---
+++ Modulo2/aula2_questao1.py
@@ -0,0 +1,11 @@
+var1 = 5
+var2 = 2
+
+
+print(var1)
+print(var2) 
+
+var3 = var1 / var2
+
+print(var3)        
+print(var3)  
---
+++ Modulo2/aula2_questao2.py
@@ -0,0 +1,15 @@
+texto = "o resultado é"
+valor1 = 10
+valor2 = 3.5
+
+resultado = valor1 + valor2
+
+
+print(texto)           
+print(texto)    
+print(valor1)          
+print(valor1) 
+print(valor2)          
+print(valor2)    
+print(resultado)      
+print(resultado) 
